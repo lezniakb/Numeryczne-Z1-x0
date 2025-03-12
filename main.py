cc93bddfc40624d9ae2cbb5ffcfb1e0bc1412313
251574 Łężniak Bartosz <251574@edu.p.lodz.pl>
2025-03-12 21:59:24 +0100

    usunięcie unimod. i poprawne sprawdzenie czy warunek bisekcji jest spelniony (+pomniejsze zmiany)

--- main.py
+++ main.py
@@ -5,26 +5,6 @@ import metoda_bisekcji as bis
 import metoda_siecznych as sie
 import rownania as r
 
-def znajdzPrzedzialUnimodalny(funkcja, poczatek, koniec, tolerancja, limitIteracji):
-    i = 0
-    znaleziono = funkcja(poczatek) * funkcja(koniec) < 0
-    odleglosc = abs(koniec - poczatek)
-    warunek = (not znaleziono) and (i < limitIteracji) and (odleglosc >= tolerancja)
-    while warunek:
-        i += 1
-        srodek = (poczatek + koniec) / float(2)
-        if funkcja(poczatek) * funkcja(srodek) < 0:
-            koniec = srodek
-        elif funkcja(srodek) * funkcja(koniec) < 0:
-            poczatek = srodek
-        else:
-            odleglosc = abs(koniec - poczatek)
-            delta = odleglosc / float(2)
-            poczatek = poczatek - delta
-            koniec = koniec + delta
-        znaleziono = (funkcja(poczatek) * funkcja(koniec) < 0)
-        warunek = (not znaleziono) and (i < limitIteracji) and (abs(koniec - poczatek) >= tolerancja)
-    return poczatek, koniec, i
 
 def rysowanieFunkcji(funkcja, poczatek, koniec, miejsceZeroweBisekcji=None, miejsceZeroweSiecznych=None):
     # Przygotowanie danych do wykresu – obliczenie wartości funkcji dla tablicy punktów
@@ -72,14 +52,13 @@ funkcjaWybrana = funkcje[wyborFunkcji][1] # funkcje.get(wyborFunkcji, funkcje[1]
 poczatekPrzedzialu = float(input("Podaj początek przedziału a: "))
 koniecPrzedzialu = float(input("Podaj koniec przedziału b: "))
 
-# Sprawdzenie założenia o przeciwnych znakach na końcach przedziału
-if funkcjaWybrana(poczatekPrzedzialu) * funkcjaWybrana(koniecPrzedzialu) >= 0:
-    print("Funkcja nie zmienia znaku na końcach przedziału.")
-    print("Próba wyznaczenia przedziału unimodalnego...")
-    poczatekPrzedzialu, koniecPrzedzialu, iterUnimodal = znajdzPrzedzialUnimodalny(funkcjaWybrana, poczatekPrzedzialu, koniecPrzedzialu, 1e-6, 50)
-    print("Znaleziono przedział: [" + str(poczatekPrzedzialu) + ", " + str(koniecPrzedzialu) + "] po " + str(iterUnimodal) + " iteracjach.")
-
 rysowanieFunkcji(funkcjaWybrana, poczatekPrzedzialu, koniecPrzedzialu)
+
+# sprawdzenie czy funkcja zmienia znak na koncach przedzialu
+if funkcjaWybrana(poczatekPrzedzialu) * funkcjaWybrana(koniecPrzedzialu) >= 0:
+    print("Funkcja nie zmienia znaku na końcach przedziału, warunek bisekcji nie został spełniony."
+          "\nProgram zostanie zakończony.")
+    exit(0)
 
 print("Wybierz kryterium stopu:")
 print("1: Osiągnięcie zadanej dokładności |f(x)| < epsilon")
@@ -92,7 +71,7 @@ if wyborKryterium == 1:
 else:
     maksIteracji = int(input("Podaj liczbę iteracji: "))
 
-# Obliczenia metodą bisekcji
+# metoda bisekcji tutaj liczona
 miejsceZeroweBisekcji, iterBisekcji = bis.metodaBisekcji(funkcjaWybrana, poczatekPrzedzialu, koniecPrzedzialu, dokladnosc, maksIteracji)
 if miejsceZeroweBisekcji is not None:
     print("\nMetoda bisekcji:")
@@ -100,7 +79,7 @@ if miejsceZeroweBisekcji is not None:
     print("Liczba iteracji: " + str(iterBisekcji))
     print("f(miejsce zerowe) = " + str(funkcjaWybrana(miejsceZeroweBisekcji)))
 
-# Obliczenia metodą siecznych
+# tutaj sieczne
 miejsceZeroweSiecznych, iterSiecznych = sie.metodaSiecznych(funkcjaWybrana, poczatekPrzedzialu, koniecPrzedzialu, dokladnosc, maksIteracji)
 if miejsceZeroweSiecznych is not None:
     print("\nMetoda siecznych:")
@@ -110,36 +89,8 @@ if miejsceZeroweSiecznych is not None:
 
 rysowanieFunkcji(funkcjaWybrana, poczatekPrzedzialu, koniecPrzedzialu, miejsceZeroweBisekcji, miejsceZeroweSiecznych)
 
-# # Przygotowanie danych do wykresu – obliczenie wartości funkcji dla tablicy punktów
-# iloscPunktow = 400
-# tablicaX = np.linspace(poczatekPrzedzialu, koniecPrzedzialu, iloscPunktow)
-# tablicaY = []
-# i = 0
-# while i < len(tablicaX):
-#     tablicaY.append(funkcjaWybrana(tablicaX[i]))
-#     i = i + 1
-#
-# # Rysowanie wykresu
-# plt.figure(figsize=(8, 6))
-# plt.plot(tablicaX, tablicaY, label="f(x)")
-# plt.axhline(0, color="black", linewidth=0.5)
-# if miejsceZeroweBisekcji is not None:
-#     plt.scatter(miejsceZeroweBisekcji, funkcjaWybrana(miejsceZeroweBisekcji), color="red", zorder=5, label="Bisekcja")
-# if miejsceZeroweSiecznych is not None:
-#     plt.scatter(miejsceZeroweSiecznych, funkcjaWybrana(miejsceZeroweSiecznych), color="blue", zorder=5, label="Sieczne")
-# plt.xlabel("x")
-# plt.ylabel("f(x)")
-# plt.title("Wykres funkcji")
-# plt.legend()
-# plt.grid(True)
-# plt.show()
-
-# DO ZROBIENIA
 """
-1. wielomiany/funkcje-tryg/etc. mają mieć współczynniki do wpisania przez użytkownika (? zapytać się na labach)
-2. dodać komentarze i zrozumieć kod
-3. zmodyfikować wykres
-4. pozmieniać np. "is not None"
-5. rozdzielić funkcje na pliki źródłowe (na górze dodać 'import drugiplik.py')
+3. zmodyfikować wykres (zmienić kropkę na trójkąt i dać przezroczyste wypełnienie)
+4. pozmieniać np. "is not None" i poprawić kod stylistycznie (wygląda sus) + dodawać zmienne pośrednie
 6. kod się miejscami powtarza
 """
